c65484ea2aa4811810c0aeca947654ccf3e198a9
Harry Yoo <harry.yoo@oracle.com>
2025-05-18 14:41:09 +0900

    add permision setup
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>

--- hypervisor-setup.sh
+++ hypervisor-setup.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+if [[ -n "${SUDO_USER:-}" ]]; then
+    orig_user="$SUDO_USER"
+else
+    orig_user="$(id -un)"
+fi
+
 sudo apt install -y \
 	qemu-kvm libvirt-daemon-system libvirt-clients \
 	bridge-utils virtinst guestfs-tools libguestfs-tools \
 	virt-manager
+
+sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
+
+sudo usermod -aG libvirt,kvm $orig_user
+sudo -u $orig_user newgrp libvirt
