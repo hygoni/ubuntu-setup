d5bb2cbe4450cf57576df422310337d6f89e0dca
Harry Yoo <harry.yoo@oracle.com>
2025-05-18 14:21:54 +0900

    Add vim and git basic setup
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>

--- build-setup.sh
+++ build-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [[ -n "${SUDO_USER:-}" ]]; then
+    orig_user="$SUDO_USER"
+else
+    orig_user="$(id -un)"
+fi
+
 # Install Necessary Packages
 
 sudo apt install -y \
@@ -8,4 +14,10 @@ sudo apt install -y \
 	libpci-dev libiberty-dev autoconf xz-utils \
 	llvm clang
 
-sudo apt install -y git
+sudo apt install -y git vim
+
+# Git setup
+
+sudo -u $orig_user git config --global core.editor vim
+sudo -u $orig_user git config --global user.name "Harry Yoo"
+sudo -u $orig_user git config --global user.email "harry.yoo@oracle.com"
