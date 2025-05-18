07504b6bda34fa763111347f73b02b726964222e
Harry Yoo <harry.yoo@oracle.com>
2025-05-18 14:06:21 +0900

    add fish installation script
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>

---
+++ fish-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt install -y fish
+if [[ -n "${SUDO_USER:-}" ]]; then
+    orig_user="$SUDO_USER"
+else
+    orig_user="$(id -un)"
+fi
+
+
+sudo usermod -s $(which fish) $orig_user
+echo -n "New Shell For $orig_user: "
+getent passwd "$orig_user" | cut -d: -f7
