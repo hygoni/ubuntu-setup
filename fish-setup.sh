6200c435febd4be4bdc85be8250dff6d5d360f05
Hyeonggon Yoo <42.hyeyoo@gmail.com>
2025-05-25 00:26:54 +0900

    Add fish timer plugin

--- fish-setup.sh
+++ fish-setup.sh
@@ -12,5 +12,9 @@ sudo usermod -s $(which fish) $orig_user
 echo -n "New Shell For $orig_user: "
 getent passwd "$orig_user" | cut -d: -f7
 
+# Copy config
 mkdir -p /home/$orig_user/.config/fish/config.fish
 cp configs/config.fish /home/$orig_user/.config/fish/config.fish
+
+# Install plugins
+wget https://raw.githubusercontent.com/jichu4n/fish-command-timer/refs/heads/master/conf.d/fish_command_timer.fish -O ~/.config/fish/conf.d/fish_command_timer.fish
